a421ce8cb638ce2a309f981b15be73956a1044cd
William Kwok <mockingod@gmail.com>
2022-03-04 01:44:38 -0800

    Added team

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -2,6 +2,9 @@ import React, { useState, useEffect } from 'react';
 import './index.css';
 import { Banner } from '../../components/banner';
 import sneaksOfNature from '../../assets/images/sneaksofnature.png';
+import { INSTAGRAM_URL, TWITTER_URL } from '../../constants';
+import { InstagramLogo, TwitterLogo } from '../../assets/icons/logos';
+import teamImg from '../../assets/images/team.png';
 
 const Home = () => {
 	const [width, setWidth] = useState(window.innerWidth);
@@ -74,17 +77,37 @@ const Home = () => {
 				</div>
 			</div>
 			)}
-			{/*
+			
+			<div className='team'>
+				<div className='team-items'>
+					<h2>THE TEAM</h2>
+					<div className='team-item'>
+						<h3>JEFF COLE</h3>
+						<p>
+							Digital artist and Co-Founder of Ikonick. Jeff Cole's art focuses mostly on pop culture and nostalgia. His art features masterful mashups of your favorite cultural icons. His consistent output of unique content has attracted attention from the biggest brands in the world, and garnered a cult-like following on Instagram.
+						</p>
+						<div className='team-item-icons'>
+							<a href={TWITTER_URL}><TwitterLogo /></a>
+							<a href={INSTAGRAM_URL}><InstagramLogo /></a>
+						</div>
+					</div>
 
-			<HomeWelcome />
-
-			<Team />
-
-            <div className='tail'>
-                <div className='tail-1'>
-                    Terms of Sale | Privacy Policy
-                </div>
-            </div> */}
+					<div className='team-item'>
+						<h3>IKONICK</h3>
+						<p>
+							The Metarelics team is powered by Ikonick, founded by Jeff Cole &amp; Mark Brazil in 2016. They are a west-coast based art brand focused on inspiring others. Their exclusive collections are in over 150,000 homes around the world. Ikonick is backed by entrepreneurial giant, Gary Vaynerchuk.
+						</p>
+						<div className='team-item-icons'>
+							<a href={TWITTER_URL}><TwitterLogo /></a>
+							<a href={INSTAGRAM_URL}><InstagramLogo /></a>
+						</div>
+					</div>
+				</div>
+				{width >= 1000 && (
+					<div className='team-picture'>
+					</div>
+				)}
+			</div>
 		</>
 	);
 }
